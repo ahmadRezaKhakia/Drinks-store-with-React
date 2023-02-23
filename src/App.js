4f1127adf80e5bac1f867d5cb4c97522498ff8c1
AhmadReza Khakia <khakia7961@gmail.com>
2023-02-23 20:18:43 +0330

    Drinks Store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+
+//import Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktails';
+import Error from './pages/Error';
+
+//import Components 
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/cocktail/:id' element={<SingleCocktail/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 }
 
